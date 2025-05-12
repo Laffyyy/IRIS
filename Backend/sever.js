531c295ee0b983de8394172bf8573144add053e3
iceyy19 <vernonmagsino26@gmail.com>
2025-05-12 23:00:52 +0800

    Setting up files
    Express is installed
    Cors is installed
    
    added
    - Controller
    - Routes
    
    modified
    - Sever.js?

--- Backend/sever.js
+++ Backend/sever.js
@@ -1,8 +1,16 @@
-const app = require('./app');
+const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
 
-const PORT = process.env.PORT || 3000;
+const userRoutes = require('./routes/userRoutes');
 
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/users', userRoutes);
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
