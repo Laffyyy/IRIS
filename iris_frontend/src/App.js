78c4a121ccb51b5fbb37056a9ec9b424dd2565fd
Laffyyy <95970405+Laffyyy@users.noreply.github.com>
2025-05-12 17:26:24 +0800

    Revert "Merge pull request #9 from Laffyyy/design/login-page-ui" git blame the docs the docs i cant see it This reverts commit 6bc8eb0b378b2f63d44e5c6f2f7d94aadf03d1d4, reversing changes made to 4ffe8376ab25183f3ca381ce0c60a79c580a7fa6.

--- iris_frontend/src/App.js
+++ iris_frontend/src/App.js
@@ -1,34 +1,23 @@
+import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
-import Login from './Login';
-import Otp from './Otp';
-import ChangePassword from './ChangePassword';
 
 function App() {
-  const [currentView, setCurrentView] = useState('login');
-
-  const renderView = () => {
-    switch (currentView) {
-      case 'login':
-        return <Login 
-          onContinue={() => setCurrentView('otp')} 
-          onForgotPassword={() => setCurrentView('changePassword')} 
-        />;
-      case 'otp':
-        return <Otp onBack={() => setCurrentView('login')} />;
-      case 'changePassword':
-        return <ChangePassword onCancel={() => setCurrentView('login')} />;
-      default:
-        return <Login 
-          onContinue={() => setCurrentView('otp')} 
-          onForgotPassword={() => setCurrentView('changePassword')} 
-        />;
-    }
-  };
-
   return (
     <div className="App">
-      {renderView()}
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
