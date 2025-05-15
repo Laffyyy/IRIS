ce74d9db011fba457e4b1d6400fc3136222aefee
Laffyyy <95970405+Laffyyy@users.noreply.github.com>
2025-05-15 20:10:52 +0800

    connection secured

--- Backend/controllers/otpcontoller.js
+++ Backend/controllers/otpcontoller.js
@@ -8,7 +8,7 @@ class OtpController {
   async sendOtp(req, res) {
     try {
       const { userId } = req.body;
-      await this.otpService.generateOtp( userId);
+      await this.otpService.generateOtp(userId);
 
       res.status(200).json({ message: 'OTP sent successfully' });
     } catch (error) {
@@ -18,3 +18,4 @@ class OtpController {
 
  
 }
+module.exports = OtpController;
