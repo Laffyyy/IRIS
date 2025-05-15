a35f63a09bf5690d54bae73c271e2eea209ee166
Laffyyy <95970405+Laffyyy@users.noreply.github.com>
2025-05-15 17:31:54 +0800

    add otp router

--- Backend/routes/otpcontoller.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const OtpController = require("../controllers/otpcontroller"); // Import the controller
-
-const otpController = new OtpController(); // Instantiate the class
-
-router.post("/send", (req, res) => otpController.sendOtp(req, res));
