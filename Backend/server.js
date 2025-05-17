9d7f02889803ff2e2c91dad83b62fc2696f7061d
iceyy19 <vernonmagsino26@gmail.com>
2025-05-17 20:26:40 +0800

    removed socketIO

--- Backend/server.js
+++ Backend/server.js
@@ -1,13 +1,9 @@
 const app = require('./app'); // Import the configured app
 const http = require('http');
-const socketIo = require('socket.io');
 const PORT = process.env.PORT || 5000;
 
 const server = http.createServer(app);
-const io = socketIo(server, { cors: { origin: '*' } });
 
-// Make io accessible in controllers via app
-app.set('io', io);
 
 server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
