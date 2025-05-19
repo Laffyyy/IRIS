13d7078a09f5d9a79832b51426df38e056633433
kierronbautista <kierronmarkbautista24@gmail.com>
2025-05-19 09:13:12 +0800

    Changes Inactivity status
    added files: InactivityHandler.js/InactivityWarningModal.css/InactivityWarning.js/LoggedOutModal.js/LoggedOutModal.css
    
    note changes will have inactivity warning when 30 secs have passed if any actions detected  will be back the user to online but 60 secs passed will throw you into forced go back to log in tab due to inactivity

--- iris_frontend/src/App.js
+++ iris_frontend/src/App.js
@@ -1,28 +1,69 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
 import Dashboard from './pages/Dashboard';
 import UserManagement from './pages/admin/UserManagement';
 import AppManagement from './pages/admin/AppManagement';
 import ClientManagement from './pages/admin/ClientManagement';
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './Login';
 import Otp from './Otp';
 import ChangePassword from './ChangePassword';
 import SecurityQuestions from './SecurityQuestions';
 import UpdatePassword from './UpdatePassword';
+import InactivityHandler from './components/InactivityHandler'; 
+
+import './App.css';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/" element={<Login />} />
+          {/* Redirect root URL to admin users */}
+          <Route path="/" element={<Navigate to="/admin/users" replace />} />
+          {/*  Add this route for the Login page */}
+          <Route path="/login" element={<Login />} />
           <Route path="/otp" element={<Otp />} />
           <Route path="/change-password" element={<ChangePassword />} />
           <Route path="/security-questions" element={<SecurityQuestions />} />
           <Route path="/update-password" element={<UpdatePassword />} />
+
+          <Route
+            path="/admin/users"
+            element={
+              <InactivityHandler>
+                <Sidebar />
+                <UserManagement />
+              </InactivityHandler>
+            }
+          />
+          <Route
+            path="/admin/apps"
+            element={
+              <InactivityHandler>
+                <Sidebar />
+                <AppManagement />
+              </InactivityHandler>
+            }
+          />
+          <Route
+            path="/admin/clients"
+            element={
+              <InactivityHandler>
+                <Sidebar />
+                <ClientManagement />
+              </InactivityHandler>
+            }
+          />
+          <Route
+            path="/dashboard"
+            element={
+              <InactivityHandler>
+                <Sidebar />
+                <Dashboard />
+              </InactivityHandler>
+            }
+          />
         </Routes>
       </div>
     </Router>
