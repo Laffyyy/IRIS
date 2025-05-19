1525261d7e48b662e322a45ca35b25e98d49be8e
Jana Zaldivar <janazaldivar03@gmail.com>
2025-05-19 10:47:05 +0800

    routes (client)

---
+++ Backend/routes/clientManagementRoutes.js
@@ -0,0 +1,27 @@
+// routes/clientManagementRoutes.js
+const express = require('express');
+const router = express.Router();
+const ClientManagementController = require('../controllers/clientManagementController');
+
+const clientManagementController = new ClientManagementController();
+
+// Client routes
+router.post('/add', (req, res) => clientManagementController.addClient(req, res));
+router.get('/getAll', (req, res) => clientManagementController.getClients(req, res));
+router.put('/update', (req, res) => clientManagementController.updateClient(req, res));
+router.post('/update', (req, res) => clientManagementController.updateClient(req, res));
+router.delete('/delete', (req, res) => clientManagementController.deleteClient(req, res));
+
+// LOB routes
+router.post('/lob/add', (req, res) => clientManagementController.addLOB(req, res));
+router.put('/lob/update', (req, res) => clientManagementController.updateLOB(req, res));
+router.post('/lob/update', (req, res) => clientManagementController.updateLOB(req, res));
+router.delete('/lob/delete', (req, res) => clientManagementController.deleteLOB(req, res));
+
+// Sub LOB routes
+router.post('/sublob/add', (req, res) => clientManagementController.addSubLOB(req, res));
+router.put('/sublob/update', (req, res) => clientManagementController.updateSubLOB(req, res));
+router.post('/sublob/update', (req, res) => clientManagementController.updateSubLOB(req, res));
+router.delete('/sublob/delete', (req, res) => clientManagementController.deleteSubLOB(req, res));
+
+module.exports = router;
