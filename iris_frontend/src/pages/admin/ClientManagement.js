a8ff7c362dddeea1fd03fa4d0cbb9e53fa4cbc0c
Jana Zaldivar <janazaldivar03@gmail.com>
2025-05-22 04:55:34 +0800

    input sanitization for deativate activate modals

--- iris_frontend/src/pages/admin/ClientManagement.js
+++ iris_frontend/src/pages/admin/ClientManagement.js
@@ -103,7 +103,9 @@ const CustomModal = ({ open, type, title, message, onConfirm, onCancel, confirmT
                 type="text"
                 value={confirmationText}
                 onChange={(e) => {
-                  setConfirmationText(e.target.value);
+                  // Only allow alphanumeric, max 30 chars
+                  let val = e.target.value.replace(/[^A-Za-z0-9]/g, '').slice(0, 30);
+                  setConfirmationText(val);
                   setError('');
                 }}
                 style={{
@@ -119,6 +121,7 @@ const CustomModal = ({ open, type, title, message, onConfirm, onCancel, confirmT
                   boxSizing: 'border-box',
                 }}
                 placeholder={inputPlaceholder}
+                maxLength={30}
                 onFocus={e => e.target.style.borderColor = '#3182ce'}
                 onBlur={e => e.target.style.borderColor = error ? '#e53e3e' : '#bdbdbd'}
                 autoComplete="off"
